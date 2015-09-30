e9b412c0778822759b67538e36ff851a453b44cc
Ben Wiedermann <benw@cs.hmc.edu>
2015-09-30 03:37:03 -0700

    Adding logging (defined inside the store class)

--- src/main/scala/store.scala
+++ src/main/scala/store.scala
@@ -7,11 +7,17 @@ class Store(initialStock: Int) {
   // properties (getters and setters)
   def widgetsInStock = inStock
   def widgetsInStock_=(newValue: Int) = {
-    require(newValue >= 0, "Out of stock!")
+    require(newValue >= 0, {log("Out of stock!"); "Out of stock!"})
     inStock = newValue
   }
 
   def order(numWidgets: Int) =  {
+    log("Order arrived...")
     widgetsInStock -= numWidgets
+    log(s"Order complete -- widgets in stock: $widgetsInStock")
+  }
+
+  def log(message: String) {
+    println(message)
   }
 }
