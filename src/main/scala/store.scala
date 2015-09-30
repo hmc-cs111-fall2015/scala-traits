b68ee5b866742f28181c7e12d2abe2862a2f25ea
Ben Wiedermann <benw@cs.hmc.edu>
2015-09-30 03:54:10 -0700

    Calling specific methods in the chain

--- src/main/scala/store.scala
+++ src/main/scala/store.scala
@@ -2,20 +2,26 @@ package acme
 
 import logging._
 
-class Store(initialStock: Int) extends TwitterLogger with ColoredConsoleLogger {
+class Store(initialStock: Int) extends ColoredConsoleLogger with TwitterLogger {
   private var inStock = 0
   widgetsInStock = initialStock
 
   // properties (getters and setters)
   def widgetsInStock = inStock
   def widgetsInStock_=(newValue: Int) = {
-    require(newValue >= 0, {log("Out of stock!"); "Out of stock!"})
+    require(newValue >= 0, 
+      {
+        super[TwitterLogger].log("All out of widgets -- check back soon!");
+        super[ColoredConsoleLogger].log("Out of stock!"); 
+        "Out of stock!"
+      })
     inStock = newValue
   }
 
   def order(numWidgets: Int) =  {
-    log("Order arrived...")
+    super[ColoredConsoleLogger].log("Order arrived...")
     widgetsInStock -= numWidgets
-    log(s"Order complete -- widgets in stock: $widgetsInStock")
+    super[ColoredConsoleLogger].log(
+      s"Order complete -- widgets in stock: $widgetsInStock")
   }
 }
